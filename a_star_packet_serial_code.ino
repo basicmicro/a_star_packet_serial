4e107ede7ab1bca2bec50c99d475ecbf4e94ba6b
BasicMicro <info@basicmicro.com>
2019-05-15 15:58:06 -0700

    Working example

--- a_star_packet_serial_code.ino
+++ a_star_packet_serial_code.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+// Include the RoboClaw Arduino library
+#include "RoboClaw.h"
 
+// Create the RoboClaw object, passing it the hardware serial pointer
+RoboClaw roboclaw(&Serial1, 10000);
+
+void setup() {
+  // Start serial communication
+  roboclaw.begin(38400);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+ // Call command on RoboClaw object, in this case turning motor 1 in the forward direction at full speed
+  roboclaw.ForwardM1(0x80, 127);
+  delay(2000);
+  roboclaw.ForwardM1(0x80, 0);
+  delay(2000);
 }
